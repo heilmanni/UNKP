2809ac4af3c82542db6d7139c0cc5d6659bbee06
heilmanni <56125449+heilmanni@users.noreply.github.com>
2020-04-20 11:44:37 +0200

    VAR modellezéssel bővítés

--- 5_Idosorok_kapcsolata.R
+++ 5_Idosorok_kapcsolata.R
@@ -1,12 +1,13 @@
-#ebben a fájlban a hangulatelemzés idõsora és a havi inflációs adatok közti kapcsolatot vizsgálom
+#ebben a fájlban a hangulatelemzés idősora és a havi inflációs adatok közti kapcsolatot vizsgálom
 
 library(lmtest)
 library(tseries)
 library(forecast)
+library(vars)
 
 #0. adatok importálása
 #szükséges: havi_index_gazdaság, havi_index, havi_origo, inflacio
-#mivel az inflációs adatok csak 2004. januártól állnak rendelkezésre, így szûkíteni kell
+#mivel az inflációs adatok csak 2004. januártól állnak rendelkezésre, így szűkíteni kell
 havi_index_gazd <- havi_index_gazd[49:237,]
 rownames(havi_index_gazd) <- c(1:189)
 havi_index <- havi_index[49:237,]
@@ -15,15 +16,14 @@ havi_origo <- havi_origo[49:237,]
 rownames(havi_origo) <- c(1:189)
 
 
-#1.A Granger-teszt feltétele, hogy mindkét idõsor stacioner legyen
+#1.A Granger-teszt feltétele, hogy mindkét idősor stacioner legyen
 
 adf.test(x = havi_index_gazd$Átlag) #stacioner
 adf.test(x = havi_index$Átlag) #stacioner, bár közel 5% a p-érték
 adf.test(x = havi_origo$Átlag) #egységgyök
 adf.test(x = inflacio$`Fogyasztóiár-index`) #egységgyök
 
-xaxis = c(1:189) #tengely
-plot(xaxis, inflacio$`Fogyasztóiár-index`, type="l", col="black")
+plot(inflacio$`Fogyasztóiár-index`, type="l", col="black")
 
 ndiffs(havi_origo$Átlag) #egyszer kell differenciázni
 ndiffs(inflacio$`Fogyasztóiár-index`) #egyszer kell differenciázni
@@ -37,6 +37,7 @@ adf.test(x = inflacio$stacioner) #már stacioner
 #2. Granger-okság
 
 #be kell source-olni a granger_test függvényt
+source(file = "https://raw.githubusercontent.com/heilmanni/UNKP/master/5_granger_test.R")
 
 granger_test(y = inflacio$stacioner, x = havi_index_gazd$Átlag, max_lag = 12)
 granger_test(y = inflacio$stacioner, x = havi_index$Átlag, max_lag = 12)
@@ -72,6 +73,28 @@ granger_test(x = inflacio$stacioner, y = havi_index_gazd$valtozas, max_lag = 12)
 granger_test(x = inflacio$stacioner, y = havi_index$valtozas, max_lag = 12)
 
 
+#VAR MODELLEK
+
+#megfelelő mátrixok létrehozása
+inf_gazd_ <- matrix(ncol=2, c(inflacio$stacioner, havi_index_gazd$valtozas)) #infláció és gazdasági hírek
+colnames(inf_gazd_) <- c("inflacio", "havi_gazd")
+
+inf_index <- matrix(ncol = 2, c(inflacio$stacioner, havi_index$valtozas)) #infláció és index hírei
+colnames(inf_index) <- c("inflacio", "index")
+
+#VAR modellezés
+#var_inf_gazd <- VAR(y = inf_gazd_, type = "const", lag.max = 12, ic = "AIC") #ha a késleltetések automatikusan szelektáltak (javasolt: 5)
+var_inf_gazd <- vars::VAR(y = inf_gazd_, type = "const", p = 5) #5 késleltetés esetén
+summary(var_inf_gazd)
+
+var_inf_gazd_8 <- vars::VAR(y = inf_gazd_, type = "const", p = 8) #8 késleltetés esetén
+summary(var_inf_gazd_8)
+
+#var_inf_index <- VAR(y = inf_index, type = "const", lag.max = 12, ic = "AIC") #ha a késleltetések automatikusan szelektáltak (javasolt: 5)
+var_inf_index <- vars::VAR(y = inf_index, type = "const", p = 5) #5 késleltetés esetén
+summary(var_inf_index)
+
+
 #KOINTEGRÁCIÓ TESZTELÉSE
 
 #az infláció és origo lineáris kapcsolatának becslése
@@ -88,7 +111,6 @@ z2 <- inflacio$`Fogyasztóiár-index` - 162.058*havi_index$Átlag
 adf.test(z2)
 
 
-xaxis = c(1:189) #tengely
-plot(xaxis, inflacio$stacioner, type="l", col="black")
-lines(xaxis, havi_index_gazd$valtozas, col="blue")
-plot(xaxis, havi_index_gazd$valtozas, col="blue", type = 'l')
+plot(inflacio$stacioner, type="l", col="black")
+lines(havi_index_gazd$valtozas, col="blue")
+plot(havi_index_gazd$valtozas, col="blue", type = 'l')
